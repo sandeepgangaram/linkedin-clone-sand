8c092a47b9a378bd56879925e4d4b7fde544a9ed
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2022-06-29 23:04:10 +0530

    removed timeout

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import clientPromise from "../../../lib/mongodb";
+
 export default NextAuth({
   providers: [
     GoogleProvider({
